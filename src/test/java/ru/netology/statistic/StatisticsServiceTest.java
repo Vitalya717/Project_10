64cb89dfb131a999e482c62aa05a16fd09694e79
Vitalya Yakovlev <vitaalya717@mail.ru>
2024-08-19 20:05:02 +0300

    adding code #2

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,23 +17,11 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    public void findMaxCell0() {
+    void findMax2() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11};
-        long expected = incomesInBillions[0];
-
-        long actual = service.findMax(incomesInBillions);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void findMaxCell10() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = incomesInBillions[10];
+        long[] incomesInBillions = {9, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
 
